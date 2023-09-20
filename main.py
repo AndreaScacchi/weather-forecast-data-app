11df8b6d442dbe3f32004e35897fda11658e2cfd
Andrea <andreascacchi10@gmail.com>
2023-09-20 11:31:25 +0200

    slider created. Title for the slider created and also an help icon is created to help users understand the functionality of the slider

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ import streamlit as st
 
 st.title("Weather Forecast for the next days")
 place = st.text_input("Place: ")
+days = st.slider("Forecast days", min_value=1, max_value=5, help="Select the number of forecasted days")
