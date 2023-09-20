9671084071f44a582bbf7db007fa78baa77b3c77
Andrea <andreascacchi10@gmail.com>
2023-09-20 15:38:29 +0200

    get_data.py file deleted. backend.py file created, and get_data function implemented to return data

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import plotly.express as px
-import get_data
+from backend import get_data
 
 st.title("Weather Forecast for the next days")
 place = st.text_input("Place: ")
@@ -8,6 +8,6 @@ days = st.slider("Forecast days", min_value=1, max_value=5, help="Select the num
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-d, t = get_data(days)
+d, t = get_data(place, days, option)
 figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
 st.plotly_chart(figure)
